c1e05611b0e1d1bf5391708837030943e49329f5
mustache1up <daniel.sp@msn.com>
2021-03-06 16:11:53 -0300

    optimization: change awk pipe by bash rematch

--- hi.sh
+++ hi.sh
@@ -17,25 +17,27 @@ hi() {
       usage
       return
   fi
+
+  ACC=0
   
   declare -a color_map;
-  color_map[0]=30 #black
-  color_map[1]=31 #red
-  color_map[2]=32 #green
-  color_map[3]=33 #yellow
+  color_map[0]=36 #cyan
+  color_map[1]=32 #green
+  color_map[2]=33 #yellow
+  color_map[3]=31 #red
   color_map[4]=34 #blue
   color_map[5]=35 #magenta
-  color_map[6]=36 #cyan
+  color_map[6]=30 #black
 
   if [[ "$#" -gt ${#color_map[@]} ]]; then
-    log_debug "#More regex patterns received than available colors."
+    # log_debug "#More regex patterns received than available colors."
     return 1
   fi
 
   IFS=$'\n'
   for line in $(cat)
   do
-    log_debug "line: $line" 
+    # log_debug "line: $line" 
 
     local ri=0
     unset charColors
@@ -47,45 +49,54 @@ hi() {
       local lineIndex=0
       local se=0-0
 
-      log_debug "regex: $regex" 
-      while true
+      # log_debug "regex: $regex" 
+      while [ ! $lineIndex -ge ${#line} ]
       do
-        [ $lineIndex -ge ${#line} ] && break
-
         local subLine=${line:${lineIndex}}
-        log_debug "lineIndex: ${lineIndex} => ${subLine}"
-        local se=$(echo "${subLine}" | awk -v "regex=$regex" ' match($0, regex) { print RSTART+1"-"RSTART+RLENGTH}')
-        log_debug "se: $se" 
-        [ -z "$se" ] && break
+        # log_debug "lineIndex: ${lineIndex} => ${subLine}"
 
-        let start=${se%-*}+$lineIndex
-        let end=${se#*-}+$lineIndex
+        STARTTIME=$(date +%s%N) ###################
+        [[ "$subLine" =~ $regex ]] || break
+        matched=${BASH_REMATCH[0]}
+        length="${#BASH_REMATCH[0]}"
+        # log_debug "BASH_REMATCH[0]: ${BASH_REMATCH[0]}"
+        # log_debug "#BASH_REMATCH[0]: ${#BASH_REMATCH[0]}"
+        before="${subLine/$matched*/''}"
+        # log_debug "before: ${before}"
+        let relative_start="${#before}"+1
+        let relative_end=relative_start+length
+
+        let start=relative_start+$lineIndex+1
+        let end=relative_end+$lineIndex
         let lineIndex=$end-1
-        log_debug "$start -> $end"
+        # log_debug "$start -> $end"
+        ENDTIME=$(date +%s%N) ###############
+        ELAPSED=$(($ENDTIME-$STARTTIME))
+        let ACC=$ACC+$ELAPSED
 
         for i in $(seq $start $end)
         do 
-          log_debug "i: ${i}" 
+          # log_debug "i: ${i}" 
           charColors[$i]="${color_map[ri]}"
         done
       done
 
-      log_debug "start: $start" 
-      log_debug "end: $end" 
+      # log_debug "start: $start" 
+      # log_debug "end: $end" 
 
-      log_debug "ri: $ri" 
-      log_debug "color_map[ri]: ${color_map[ri]}" 
-      log_debug "${#r}: ${#r}" 
+      # log_debug "ri: $ri" 
+      # log_debug "color_map[ri]: ${color_map[ri]}" 
+      # log_debug "${#r}: ${#r}" 
 
       let ri++
     done
 
     local outputLine=""
     
-    log_debug "$(declare -p charColors)"
+    # log_debug "$(declare -p charColors)"
 
     local firstKey=("${!charColors[@]}")
-    log_debug "firstKey: ${firstKey}" 
+    # log_debug "firstKey: ${firstKey}" 
 
     local lastLineIndex=0
 
@@ -93,7 +104,7 @@ hi() {
     local colorStart=${firstKey}
     local colorEnd=${firstKey}
 
-    log_debug "color: ${color}" 
+    # log_debug "color: ${color}" 
 
     for charColorIndex in "${!charColors[@]}"
     do
@@ -101,9 +112,9 @@ hi() {
       let diff=charColorIndex-colorEnd
       if [[ "$color" != "$charColor" ]] || [ $diff -gt 1 ]
       then
-        log_debug "color=${color}" 
-        log_debug "colorStart=${colorStart}" 
-        log_debug "colorEnd=${colorEnd}" 
+        # log_debug "color=${color}" 
+        # log_debug "colorStart=${colorStart}" 
+        # log_debug "colorEnd=${colorEnd}" 
 
         outputLine="${outputLine}${line:lastLineIndex:colorStart-2-lastLineIndex}"
         outputLine="${outputLine}$(echo -e "\e[1;${color}m")"
@@ -118,13 +129,13 @@ hi() {
       fi
 
       colorEnd=$charColorIndex
-      log_debug "colorEnd: ${colorEnd}" 
+      # log_debug "colorEnd: ${colorEnd}" 
     done
 
 
-    log_debug "color: ${color}" 
-    log_debug "colorStart: ${colorStart}" 
-    log_debug "colorEnd: ${colorEnd}" 
+    # log_debug "color: ${color}" 
+    # log_debug "colorStart: ${colorStart}" 
+    # log_debug "colorEnd: ${colorEnd}" 
 
     outputLine="${outputLine}${line:lastLineIndex:colorStart-2-lastLineIndex}"
     outputLine="${outputLine}$(echo -e "\e[1;${color}m")"
@@ -134,8 +145,11 @@ hi() {
     lastLineIndex=${colorEnd}-1
     outputLine="${outputLine}${line:lastLineIndex}"
 
-    log_debug "line: $line" 
+    # log_debug "line: $line" 
     echo "$outputLine"
   done
+
+
+  echo "It takes $((${ACC}/1000000)) ticks to complete all computed task..." >&2  ###########
 
 }
--- unit_test.sh
+++ unit_test.sh
@@ -2,43 +2,45 @@
 
 source ./hi.sh
 
+export DEBUG=true
+
 testEquality() {
   assertEquals 1 1
 }
 
 testSingleWordRegex() {
-  actual="$(echo "aaabbbccc" | hi b 2> /dev/null)"
-  expected="$(echo -e "aaa""\e[1;30m""bbb""\e[0m""ccc")"
+  actual="$(echo "aaabbbccc" | hi b )"
+  expected="$(echo -e "aaa""\e[1;36m""bbb""\e[0m""ccc")"
   assertEquals "${expected}" "${actual}" 
 }
 
 testSinglePatternRegex() {
-  actual="$(echo "aaabbbccc" | hi b+ 2> /dev/null)"
-  expected="$(echo -e "aaa""\e[1;30m""bbb""\e[0m""ccc")"
+  actual="$(echo "aaabbbccc" | hi b+ )"
+  expected="$(echo -e "aaa""\e[1;36m""bbb""\e[0m""ccc")"
   assertEquals "${expected}" "${actual}" 
 }
 
 testTwoRegexes() {
-  actual="$(echo "aaabbbcccddd" | hi b+ c+ 2> /dev/null)"
-  expected="$(echo -e "aaa""\e[1;30m""bbb""\e[0m""\e[1;31m""ccc""\e[0m""ddd")"
+  actual="$(echo "aaabbbcccddd" | hi b+ c+ )"
+  expected="$(echo -e "aaa""\e[1;36m""bbb""\e[0m""\e[1;32m""ccc""\e[0m""ddd")"
   assertEquals "${expected}" "${actual}" 
 }
 
 testTwoOverlapingRegexesShorterAfter() {
-  actual="$(echo "aaabbbcccddd" | hi [bc]+ bc 2> /dev/null)"
-  expected="$(echo -e "aaa""\e[1;30m""bb""\e[0m""\e[1;31m""bc""\e[0m""\e[1;30m""cc""\e[0m""ddd")"
+  actual="$(echo "aaabbbcccddd" | hi [bc]+ bc )"
+  expected="$(echo -e "aaa""\e[1;36m""bb""\e[0m""\e[1;32m""bc""\e[0m""\e[1;36m""cc""\e[0m""ddd")"
   assertEquals "${expected}" "${actual}" 
 }
 
 testTwoOverlapingRegexesLongerAfter() {
-  actual="$(echo "aaabbbcccddd" | hi bc [bc]+ 2> /dev/null)"
-  expected="$(echo -e "aaa""\e[1;31m""bbbccc""\e[0m""ddd")"
+  actual="$(echo "aaabbbcccddd" | hi bc [bc]+ )"
+  expected="$(echo -e "aaa""\e[1;32m""bbbccc""\e[0m""ddd")"
   assertEquals "${expected}" "${actual}" 
 }
 
 testSinglePatternRegexMultipleOccurences() {
-  actual="$(echo "aaabbbcccbbb" | hi b+ 2> /dev/null)"
-  expected="$(echo -e "aaa""\e[1;30m""bbb""\e[0m""ccc""\e[1;30m""bbb""\e[0m")"
+  actual="$(echo "aaabbbcccbbb" | hi b+ )"
+  expected="$(echo -e "aaa""\e[1;36m""bbb""\e[0m""ccc""\e[1;36m""bbb""\e[0m")"
   assertEquals "${expected}" "${actual}" 
 }
 
